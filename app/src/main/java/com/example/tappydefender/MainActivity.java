9291dd8d6c3555c58718ec111c4d92be992e3633
sidharth nayyar <sidnayyar1@gmail.com>
2020-06-24 11:18:12 -0400

    Added the onclick listener in main activity and added images in project

--- app/src/main/java/com/example/tappydefender/MainActivity.java
+++ app/src/main/java/com/example/tappydefender/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.tappydefender;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+
+    final Button buttonPlay =
+            (Button)findViewById(R.id.buttonPlay);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonPlay.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
